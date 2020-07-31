7d290277c69d55449f06f41dad5a9bdd92f8b5d1
Jaewoong Lee <230@fumire.moe>
2020-08-01 01:24:16 +0900

    Get name data

--- crawling/stock.py
+++ crawling/stock.py
@@ -16,14 +16,14 @@ while True:
         for _, row1 in investpy.get_stocks(country=country).iterrows():
             print("-", row1["name"])
             for date, row2 in investpy.get_stock_historical_data(stock=row1["symbol"], country=country, from_date="01/01/1900", to_date=datetime.datetime.today().strftime("%d/%m/%Y"), order="descending").iterrows():
-                query = "SELECT * FROM `StockData` WHERE `country` LIKE '%s' AND `Name` LIKE '%s' AND `Symbol` LIKE '%s' AND `Date` = '%s'"
-                cursor.execute(query, (country, row1["name"], row1["symbol"], date.date()))
+                query = "SELECT * FROM `StockData` WHERE `country` LIKE '%s' AND `Name` LIKE '%s' AND `Symbol` LIKE '%s' AND `Date` = '%s'" % (country, row1["name"], row1["symbol"], date.date())
+                cursor.execute(query)
 
                 if cursor.fetchall():
                     break
 
-                query = "INSERT INTO `StockData` (`IndexColumn`, `Country`, `Name`, `Symbol`, `Date`, `Open`, `High`, `Low`, `Close`, `Volume`, `Currency`) VALUES (NULL, '%s', '%s', '%s', '%s', '%f', '%f', '%f', '%f', '%d', '%s');"
-                cursor.execute(query, (country, row1["name"], row1["symbol"], date.date(), row2["Open"], row2["High"], row2["Low"], row2["Close"], row2["Volume"], row2["Currency"]))
+                query = "INSERT INTO `StockData` (`IndexColumn`, `Country`, `Name`, `Symbol`, `Date`, `Open`, `High`, `Low`, `Close`, `Volume`, `Currency`) VALUES (NULL, '%s', '%s', '%s', '%s', '%f', '%f', '%f', '%f', '%d', '%s');" % (country, row1["name"], row1["symbol"], date.date(), row2["Open"], row2["High"], row2["Low"], row2["Close"], row2["Volume"], row2["Currency"])
+                cursor.execute(query)
 
                 print("--", date.date())
 
