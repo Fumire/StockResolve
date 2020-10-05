6184de51f2466956e14d712647a5db96cc95eb4c
Jaewoong Lee <230@fumire.moe>
2020-10-05 20:06:50 +0900

    Estimate tomorrow: because it is too slow

--- estimation/estimation.py
+++ estimation/estimation.py
@@ -8,7 +8,7 @@ import pymysql
 
 while True:
     hour = time.localtime().tm_hour
-    if not (10 <= hour <= 16):
+    if not (hour == 0):
         time.sleep(60)
         continue
 
@@ -20,13 +20,11 @@ while True:
     cursor.execute(sql)
 
     for name, code in sorted(map(lambda x: (x["Name"], "%06d" % int(x["Symbol"])), cursor.fetchall())):
-        current_time = datetime.datetime.now().time()
-
         sql = "SELECT DISTINCT cast(AddedTime as date) FROM `KRXData` WHERE `Name` LIKE %s ORDER BY cast(AddedTime as date) ASC"
         cursor.execute(sql, (name,))
         result = cursor.fetchall()
 
-        if len(result) == 1:
+        if len(result) < 2:
             continue
 
         days = sorted(map(lambda x: x["cast(AddedTime as date)"], result))
@@ -52,33 +50,26 @@ while True:
             one_data.sort_index(inplace=True)
             one_data = one_data.asfreq(freq="T")
             one_data.drop(labels=list(filter(lambda x: x.time() > datetime.time(15, 30), one_data.index)), axis="index", inplace=True)
-            one_data.drop(labels=list(filter(lambda x: x.time() > current_time, one_data.index)), axis="index", inplace=True)
             one_data.interpolate(method="time", inplace=True)
             closed_prices.append(list(one_data.iloc[-1])[0])
             one_data.index = list(map(lambda x: x.time(), one_data.index))
 
             whole_data = whole_data.join(one_data, how="right")
 
-        if whole_data.shape[0] <= 1:
+        if whole_data.shape[1] <= 2:
             continue
 
         whole_data = whole_data.T
+        closed_prices = closed_prices[1:]
         print(whole_data)
 
-        regressor = sklearn.ensemble.RandomForestRegressor(max_features=None, bootstrap=False, n_jobs=-1, random_state=0)
-        regressor.fit(whole_data.iloc[:-1], closed_prices[:-1])
+        regressor = sklearn.ensemble.RandomForestRegressor(max_features=None, bootstrap=False, n_jobs=1, random_state=0)
+        regressor.fit(whole_data.iloc[:-1], closed_prices)
 
-        estimation_value, real_value = regressor.predict(whole_data.iloc[[-1]])[0], closed_prices[-1]
+        estimation_value = regressor.predict(whole_data.iloc[[-1]])[0]
 
-        sql = "SELECT * FROM `Estimations` WHERE `Name` LIKE %s AND `Code` LIKE %s AND `EstimatedDate` = %s"
-        cursor.execute(sql, (name, code, days[-1]))
-
-        if cursor.fetchall():
-            sql = "UPDATE `Estimations` SET `Estimation` = %s, RealValue = %s WHERE `Name` LIKE %s AND `Code` LIKE %s AND `EstimatedDate` = %s"
-            cursor.execute(sql, (str(estimation_value), str(real_value), name, code, days[-1]))
-        else:
-            sql = "INSERT INTO `Estimations` (`Name`, `Code`, `EstimatedDate`, `Estimation`, `RealValue`) VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(sql, (name, code, days[-1], str(estimation_value), str(real_value)))
+        sql = "INSERT INTO `Estimations` (`Name`, `Code`, `EstimatedDate`, `Estimation`, `RealValue`) VALUES (%s, %s, CURRENT_DATE, %s, 0)"
+        cursor.execute(sql, (name, code, str(estimation_value)))
 
         print(name, "Done!!")
 
---
+++ estimation/result.py
@@ -0,0 +1,31 @@
+import time
+import pymysql
+
+while True:
+    hour = time.localtime().tm_hour
+    if not (hour == 16):
+        time.sleep(60)
+        continue
+
+    with open("/password1.txt", "r") as f:
+        connection = pymysql.connect(host="fumire.moe", user="fumiremo_stock", password=f.readline().strip(), db="fumiremo_StockDB", charset="utf8", port=3306)
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+    sql = "SELECT `Name` FROM `Estimations` WHERE `EstimatedDate` = CURRENT_DATE ORDER BY `Name` ASC"
+    cursor.execute(sql)
+
+    for name in sorted(map(lambda x: x["Name"], cursor.fetchall())):
+        sql = "SELECT `price` FROM `KRXData` WHERE `Name` LIKE %s AND cast(`AddedTime` as date) = CURRENT_DATE ORDER BY `AddedTime` ASC LIMIT 1"
+        cursor.execute(sql, (name,))
+        start = cursor.fetchone()["price"]
+
+        sql = "SELECT `price` FROM `KRXData` WHERE `Name` LIKE %s AND cast(`AddedTime` as date) = CURRENT_DATE ORDER BY `AddedTime` DESC LIMIT 1"
+        cursor.execute(sql, (name,))
+        end = cursor.fetchone()["price"]
+
+        sql = "UPDATE `Estimations` SET `RealValue` = %s WHERE `Name` LIKE %s AND `EstimatedDate` = CURRENT_DATE"
+        cursor.execute(sql, (str(end / start), name))
+
+        print(name, "Done!!")
+
+    connection.close()
