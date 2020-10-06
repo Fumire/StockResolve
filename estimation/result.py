e61d3a73d19a2eb3ffb7fdce12ef6396ca8552f1
Jaewoong Lee <230@fumire.moe>
2020-10-06 13:27:24 +0900

    handling error

--- estimation/result.py
+++ estimation/result.py
@@ -15,17 +15,20 @@ while True:
     cursor.execute(sql)
 
     for name in sorted(map(lambda x: x["Name"], cursor.fetchall())):
-        sql = "SELECT `price` FROM `KRXData` WHERE `Name` LIKE %s AND cast(`AddedTime` as date) = CURRENT_DATE ORDER BY `AddedTime` ASC LIMIT 1"
-        cursor.execute(sql, (name,))
-        start = cursor.fetchone()["price"]
+        try:
+            sql = "SELECT `price` FROM `KRXData` WHERE `Name` LIKE %s AND cast(`AddedTime` as date) = CURRENT_DATE ORDER BY `AddedTime` ASC LIMIT 1"
+            cursor.execute(sql, (name,))
+            start = cursor.fetchone()["price"]
 
-        sql = "SELECT `price` FROM `KRXData` WHERE `Name` LIKE %s AND cast(`AddedTime` as date) = CURRENT_DATE ORDER BY `AddedTime` DESC LIMIT 1"
-        cursor.execute(sql, (name,))
-        end = cursor.fetchone()["price"]
+            sql = "SELECT `price` FROM `KRXData` WHERE `Name` LIKE %s AND cast(`AddedTime` as date) = CURRENT_DATE ORDER BY `AddedTime` DESC LIMIT 1"
+            cursor.execute(sql, (name,))
+            end = cursor.fetchone()["price"]
 
-        sql = "UPDATE `Estimations` SET `RealValue` = %s WHERE `Name` LIKE %s AND `EstimatedDate` = CURRENT_DATE"
-        cursor.execute(sql, (str(end / start), name))
+            sql = "UPDATE `Estimations` SET `RealValue` = %s WHERE `Name` LIKE %s AND `EstimatedDate` = CURRENT_DATE"
+            cursor.execute(sql, (str(end / start), name))
 
-        print(name, "Done!!")
+            print(name, "Done!!")
+        except TypeError:
+            continue
 
     connection.close()
